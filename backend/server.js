360031ae73fb06f280181beb45bb69f90844368c
Nandhakumar2005 <nandhakumarks2028@cs.sjcetpalai.ac.in>
2026-04-05 14:29:20 +0530

    backend updated

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,32 @@
 require("dotenv").config({ path: "../.env" });
 
 const express = require("express");
-const connectDB = require("./config/db");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-connectDB();
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
-// Routes
-app.use("/api/expenses", require("./routes/expenseRoutes"));
+// Import routes
+const expenseRoutes = require("./routes/expenseRoutes");
+
+// Use routes
+app.use("/api/expenses", expenseRoutes);
 
 // Test route
 app.get("/", (req, res) => {
-    res.send("Server is running");
+  res.send("Server is running");
 });
 
 // Start server
 app.listen(3000, () => {
-    console.log("Server running on port 3000");
+  console.log("Server running on port 3000");
 });
